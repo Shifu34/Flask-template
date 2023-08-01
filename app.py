08d06b81239ba1011cf559b2fc3d77219fe925c4
Shafqat Mehmood <shafqat129.mehmood@gmail.com>
2023-08-01 12:19:35 +0500

    Flask related code added

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask,render_template
+import aiml 
+import getStats
+
+
+app=Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/cars') #different pages
+def products():
+    return 'This is page of car'
+
+@app.route('/getStats') #different pages
+def products():
+    return aiml.getStats()
+
+
+if __name__=="__main__":
+    app.run(debug=True)
